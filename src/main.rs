edf747605e1b8db19478aaa17370f8c9896d80ff
Martin Pool <mbp@sourcefrog.net>
2021-09-12 08:47:34 -0700

    Split out textedit code

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::env::args;
 use std::path::PathBuf;
 
 use anyhow::Result;
-use proc_macro2::{LineColumn, Span};
+use proc_macro2::Span;
+use syn::ItemFn;
 // use syn::parse;
 // use quote::ToTokens;
 use syn::visit::Visit;
-use syn::ItemFn;
+
+mod textedit;
+use textedit::replace_line_column_region;
 
 #[derive(Debug, Eq, PartialEq)]
 enum MutationOp {
@@ -31,43 +34,10 @@ impl Mutation {
                 source,
                 &self.span.start(),
                 &self.span.end(),
-                "{\n /* ~~ removed by enucleate ~~ */ Default::default()\n}",
+                "{\n /* ~ removed by enucleate ~ */ Default::default()\n}",
             ),
         }
     }
-}
-
-/// Return s with the specified inclusive region replaced.
-///
-/// In `LineColumn`, lines are 1-indexed, and inclusive; columns are 0-indexed
-/// in UTF-8 characters (presumably really code points) and inclusive.
-fn replace_line_column_region(
-    s: &str,
-    start: &LineColumn,
-    end: &LineColumn,
-    replacement: &str,
-) -> String {
-    let mut r = String::with_capacity(s.len());
-    let mut line_no = 1;
-    let mut col_no = 0;
-    for c in s.chars() {
-        if line_no < start.line
-            || line_no > end.line
-            || (line_no == start.line && col_no < start.column)
-            || (line_no == end.line && col_no > end.column)
-        {
-            r.push(c);
-        } else if line_no == start.line && col_no == start.column {
-            r.push_str(replacement);
-        }
-        if c == '\n' {
-            line_no += 1;
-            col_no = 0;
-        } else {
-            col_no += 1;
-        }
-    }
-    r
 }
 
 #[derive(Default, Debug)]
@@ -111,7 +81,7 @@ fn main() -> Result<()> {
     let mutation_sites = discover_mutation_sites(&file_ast);
     // eprintln!("{:#?}", mutation_sites);
     for m in &mutation_sites[..1] {
-        print!("{}",m.apply(&code));
+        print!("{}", m.apply(&code));
     }
     // let out_tokens = file_ast.into_token_stream();
     // println!("{}", out_tokens);
@@ -120,55 +90,5 @@ fn main() -> Result<()> {
 
 #[cfg(test)]
 mod test {
-    use super::*;
-
-    #[test]
-    fn replace_region() {
-        let source = "
-fn foo() {
-    some();
-    stuff();
-}
-
-const BAR: u32 = 32;
-";
-        // typical multi-line case
-        assert_eq!(
-            replace_line_column_region(
-                &source,
-                &LineColumn { line: 2, column: 9 },
-                &LineColumn { line: 5, column: 0 },
-                "{ /* body deleted */ }"
-            ),
-            "
-fn foo() { /* body deleted */ }
-
-const BAR: u32 = 32;
-"
-        );
-
-        // single-line case
-        assert_eq!(
-            replace_line_column_region(
-                &source,
-                &LineColumn {
-                    line: 7,
-                    column: 17
-                },
-                &LineColumn {
-                    line: 7,
-                    column: 18
-                },
-                "69"
-            ),
-            "
-fn foo() {
-    some();
-    stuff();
-}
-
-const BAR: u32 = 69;
-"
-        );
-    }
+    // use super::*;
 }
---
+++ src/textedit.rs
@@ -0,0 +1,91 @@
+// Copyright 2021 Martin Pool
+
+use proc_macro2::LineColumn;
+
+/// Return s with the specified inclusive region replaced.
+///
+/// In `LineColumn`, lines are 1-indexed, and inclusive; columns are 0-indexed
+/// in UTF-8 characters (presumably really code points) and inclusive.
+pub(crate) fn replace_line_column_region(
+    s: &str,
+    start: &LineColumn,
+    end: &LineColumn,
+    replacement: &str,
+) -> String {
+    let mut r = String::with_capacity(s.len() + replacement.len());
+    let mut line_no = 1;
+    let mut col_no = 0;
+    for c in s.chars() {
+        if line_no < start.line
+            || line_no > end.line
+            || (line_no == start.line && col_no < start.column)
+            || (line_no == end.line && col_no > end.column)
+        {
+            r.push(c);
+        } else if line_no == start.line && col_no == start.column {
+            r.push_str(replacement);
+        }
+        if c == '\n' {
+            line_no += 1;
+            col_no = 0;
+        } else {
+            col_no += 1;
+        }
+    }
+    r
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn replace_region() {
+        let source = "
+fn foo() {
+    some();
+    stuff();
+}
+
+const BAR: u32 = 32;
+";
+        // typical multi-line case
+        assert_eq!(
+            replace_line_column_region(
+                &source,
+                &LineColumn { line: 2, column: 9 },
+                &LineColumn { line: 5, column: 0 },
+                "{ /* body deleted */ }"
+            ),
+            "
+fn foo() { /* body deleted */ }
+
+const BAR: u32 = 32;
+"
+        );
+
+        // single-line case
+        assert_eq!(
+            replace_line_column_region(
+                &source,
+                &LineColumn {
+                    line: 7,
+                    column: 17
+                },
+                &LineColumn {
+                    line: 7,
+                    column: 18
+                },
+                "69"
+            ),
+            "
+fn foo() {
+    some();
+    stuff();
+}
+
+const BAR: u32 = 69;
+"
+        );
+    }
+}
