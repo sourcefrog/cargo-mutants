e9c2b2be99b548a74326d889e832d1fdb2d339da
Martin Pool <mbp@sourcefrog.net>
2021-09-12 08:32:06 -0700

    Add an example tree in testdata

---
+++ testdata/tree/factorial/src/bin/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for i in 1..=6 {
+        println!("{}! = {}", i, factorial(i));
+    }
+}
+
+fn factorial(n: u32) -> u32 {
+    let mut a = 1;
+    for i in 2..=n {
+        a *= i;
+    }
+    a
+}
