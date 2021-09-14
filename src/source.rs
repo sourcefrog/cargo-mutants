3d7b653226727156af7735725fcf8bffdeae435d
Martin Pool <mbp@sourcefrog.net>
2021-09-14 10:41:36 -0700

    Update source.rs

--- src/source.rs
+++ src/source.rs
@@ -40,7 +40,7 @@ mod test {
         assert!(
             SourceTree::new(Path::new("testdata/tree/factorial"))
                 .source_files()
-                .eq([Path::new("src/bin/main.rs")].iter())
+                .eq([Path::new("src/bin/main.rs")].into_iter())
         )
     }
 }
