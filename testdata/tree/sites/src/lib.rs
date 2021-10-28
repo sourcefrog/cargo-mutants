e43d24f1146bf94586c0ffdfd822357c6890f002
Martin Pool <mbp@sourcefrog.net>
2021-10-27 17:19:21 -0700

    More mutation site tests

--- testdata/tree/sites/src/lib.rs
+++ testdata/tree/sites/src/lib.rs
@@ -5,10 +5,24 @@
 
 /// This function is only built for tests so shouldn't be mutated.
 #[cfg(test)]
-fn outer_test_helper() {}
+fn outer_test_helper() {
+    panic!()
+}
+
+/// Can be mutated to return default (0).
+fn returns_42u32() -> u32 {
+    42
+}
+
+/// Can be mutated to return bool::default.
+fn returns_true() -> bool {
+    true
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     /// A test helper function: it shouldn't be mutated because it's inside a
     /// `#[cfg(tests)]` mod.
     fn test_helper() -> usize {
@@ -19,6 +33,12 @@ mod tests {
     fn use_test_helper() {
         let result = 2 + test_helper();
         assert_eq!(result, 44);
+    }
+
+    #[test]
+    fn test_mutatable_functions() {
+        assert_eq!(returns_42u32(), 42);
+        assert!(returns_true());
     }
 }
 
