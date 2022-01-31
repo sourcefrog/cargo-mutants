d089f90a3a48a8e4bd4abd3bdb36a27c5e8e27fa
Martin Pool <mbp@sourcefrog.net>
2022-01-30 18:00:56 -0800

    Fix Windows CI: don't try to setpgid
    This seems to only build on Unix

--- src/run.rs
+++ src/run.rs
@@ -45,9 +45,8 @@ pub fn run_cargo(
             stdin: Redirection::None,
             stdout: Redirection::File(log_file.open_append()?),
             stderr: Redirection::Merge,
-            setpgid: true,
             cwd: Some(in_dir.as_os_str().to_owned()),
-            ..Default::default()
+            ..setpgid_on_unix()
         },
     )
     .with_context(|| format!("failed to spawn {} {}", cargo_bin, cargo_args.join(" ")))?;
@@ -83,4 +82,17 @@ pub fn run_cargo(
     } else {
         Ok(CargoResult::Failure)
     }
+}
+
+#[cfg(unix)]
+fn setpgid_on_unix() -> PopenConfig {
+    PopenConfig {
+        setpgid: true,
+        ..Default::default()
+    }
+}
+
+#[cfg(not(unix))]
+fn setpgid_on_unix() -> PopenConfig {
+    ..Default::default()
 }
