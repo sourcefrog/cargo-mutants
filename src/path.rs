1696e02343a9ad0a411538cbae00c44b1789e8af
Martin Pool <mbp@sourcefrog.net>
2022-07-17 18:00:11 -0700

    Avoid double slash in Utf8PathSlashes
    (and fix a previously missed mutation site)

--- src/path.rs
+++ src/path.rs
@@ -8,7 +8,23 @@ impl Utf8PathSlashes for Utf8Path {
     fn to_slash_path(&self) -> String {
         self.components()
             .map(|c| c.as_str())
+            .filter(|c| !c.is_empty())
+            .map(|c| if c == "/" {"" } else { c })
             .collect::<Vec<_>>()
             .join("/")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use camino::Utf8PathBuf;
+
+    use super::Utf8PathSlashes;
+
+    #[test]
+    fn path_slashes_drops_empty_parts() {
+        let mut path = Utf8PathBuf::from("/a/b/c/");
+        path.push("d/e/f");
+        assert_eq!(path.to_slash_path(), "/a/b/c/d/e/f");
     }
 }
