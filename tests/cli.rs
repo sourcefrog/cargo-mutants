6fec6a3c35a68f1a638a8a6f102f8482bc834aec
Martin Pool <mbp@sourcefrog.net>
2022-09-19 17:44:31 -0700

    Test different INSTA_UPDATE values

--- tests/cli.rs
+++ tests/cli.rs
@@ -1145,19 +1145,23 @@ fn strict_warnings_about_unused_variables_are_disabled_so_mutants_compile() {
         .stdout(contains("1 mutant tested: 1 caught"));
 }
 
+/// `INSTA_UPDATE=always` in the environment will cause Insta to update
+/// the snaphots, so the tests will pass, so mutants will not be caught.
+/// This test checks that cargo-mutants sets the environment variable
+/// so that mutants are caught properly.
 #[test]
-fn insta_tree_succeeds_and_leaves_no_snaphots() {
-    let tmp_src_dir = copy_of_testdata("insta");
-    run_assert_cmd()
-        .arg("mutants")
-        .args(["--no-times", "--no-shuffle", "--caught"])
-        .current_dir(tmp_src_dir.path())
-        .assert()
-        .success();
-    let caught_txt = fs::read_to_string(tmp_src_dir.path().join("mutants.out/caught.txt")).unwrap();
-    insta::assert_snapshot!(caught_txt);
-    // TODO: Check that there are no `.new` files in the source directory.
-    // TODO: Run this again with INSTA_UPDATE=always.
+fn insta_test_failures_are_detected() {
+    for insta_update in ["auto", "always"] {
+        println!("INSTA_UPDATE={insta_update}");
+        let tmp_src_dir = copy_of_testdata("insta");
+        run_assert_cmd()
+            .arg("mutants")
+            .args(["--no-times", "--no-shuffle", "--caught"])
+            .env("INSTA_UPDATE", insta_update)
+            .current_dir(tmp_src_dir.path())
+            .assert()
+            .success();
+    }
 }
 
 fn check_text_list_output(dir: &Path, test_name: &str) {
