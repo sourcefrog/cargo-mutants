552aaee434ede9e737b7f31d52c59a0ac097d6af
Martin Pool <mbp@sourcefrog.net>
2022-09-27 21:46:48 -0700

    Catch "replace attr_is_mutants_skip::list_is_mutants_skip -> bool with true"
    #mutants

--- testdata/tree/cfg_attr_test_skip/src/lib.rs
+++ testdata/tree/cfg_attr_test_skip/src/lib.rs
@@ -12,8 +12,19 @@ pub fn factorial(n: u32) -> u32 {
     a
 }
 
+// This has a cfg_attr, but not one that we should match
+#[cfg_attr(feature = "magic", sparkles, crackles)]
+pub fn double(x: usize) -> usize {
+    2 * x
+}
+
 #[test]
 fn test_factorial() {
     println!("factorial({}) = {}", 6, factorial(6)); // This line is here so we can see it in --nocapture
     assert_eq!(factorial(6), 720);
+}
+
+#[test]
+fn test_double() {
+    assert_eq!(double(21), 42);
 }
