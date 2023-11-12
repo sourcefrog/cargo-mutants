27b10426fd3f2c8218aa9238231128bd258f5a5f
Martin Pool <mbp@sourcefrog.net>
2023-11-12 07:22:34 -0800

    Tests and fixes for affected-line logic

--- src/diff_filter.rs
+++ src/diff_filter.rs
@@ -12,6 +12,7 @@ use camino::Utf8Path;
 use itertools::Itertools;
 use patch::{Line, Patch, Range};
 use tracing::{trace, warn};
+use tracing_subscriber::field::debug;
 
 use crate::mutate::Mutant;
 use crate::Result;
@@ -25,7 +26,7 @@ pub fn diff_filter(mutants: Vec<Mutant>, diff_text: &str) -> Result<Vec<Mutant>>
     for patch in &patches {
         let path = strip_patch_path(&patch.new.path);
         if lines_changed_by_path
-            .insert(path, remove_context(patch))
+            .insert(path, affected_lines(patch))
             .is_some()
         {
             bail!("Patch input contains repeated filename: {path:?}");
@@ -48,8 +49,7 @@ pub fn diff_filter(mutants: Vec<Mutant>, diff_text: &str) -> Result<Vec<Mutant>>
                         ?path,
                         line,
                         %mutant,
-                        mutant_span = ?&mutant.span,
-                        "diff matched mutant"
+                    "diff matched mutant"
                     );
                     matched.push(mutant);
                     continue 'mutant;
@@ -76,11 +76,30 @@ fn strip_patch_path(path: &str) -> &Utf8Path {
 /// (A list of ranges would be more concise but this is easier for a first version.)
 ///
 /// If a line is deleted then the range will span from the line before to the line after.
-fn remove_context(patch: &Patch) -> Vec<usize> {
+fn affected_lines(patch: &Patch) -> Vec<usize> {
     let mut r = Vec::new();
     for hunk in &patch.hunks {
         let mut lineno: usize = hunk.new_range.start.try_into().unwrap();
+        // True if the previous line was deleted. If set, then the next line that exists in the
+        // new file, if there is one, will be marked as affected.
+        let mut prev_removed = false;
         for line in &hunk.lines {
+            match line {
+                Line::Remove(_) => {
+                    prev_removed = true;
+                }
+                Line::Add(_) | Line::Context(_) => {
+                    if prev_removed {
+                        debug_assert!(
+                            r.last().map_or(true, |last| *last < lineno),
+                            "{lineno} {r:?}"
+                        );
+                        debug_assert!(lineno >= 1, "{lineno}");
+                        r.push(lineno);
+                        prev_removed = false;
+                    }
+                }
+            }
             match line {
                 Line::Context(_) => {
                     lineno += 1;
@@ -95,9 +114,6 @@ fn remove_context(patch: &Patch) -> Vec<usize> {
                     if lineno > 1 && r.last().map_or(true, |last| *last < (lineno - 1)) {
                         r.push(lineno - 1);
                     }
-                    if lineno > 0 && r.last().map_or(true, |last| *last < lineno) {
-                        r.push(lineno);
-                    }
                 }
             }
         }
@@ -111,7 +127,9 @@ fn remove_context(patch: &Patch) -> Vec<usize> {
 
 #[cfg(test)]
 mod test_super {
+    use assert_cmd::assert;
     use pretty_assertions::assert_eq;
+    use similar::TextDiff;
 
     use super::*;
 
@@ -146,5 +164,103 @@ index eb42779..a0091b7 100644
 ";
         let filtered: Vec<Mutant> = diff_filter(Vec::new(), diff).expect("diff filtered");
         assert_eq!(filtered.len(), 0);
+    }
+
+    fn make_diff(old: &str, new: &str) -> String {
+        TextDiff::from_lines(old, new)
+            .unified_diff()
+            .context_radius(2)
+            .header("a/file", "b/file")
+            .to_string()
+    }
+
+    #[test]
+    fn strip_patch_path_prefix() {
+        assert_eq!(strip_patch_path("b/src/mutate.rs"), "src/mutate.rs");
+    }
+
+    #[test]
+    fn affected_lines_from_single_insertion() {
+        let orig_lines = (1..=4).map(|i| format!("line {}\n", i)).collect_vec();
+        for i in 1..=5 {
+            let mut new = orig_lines.clone();
+            let new_value = "new line\n".to_owned();
+            if i < 5 {
+                new.insert(i - 1, new_value);
+            } else {
+                new.push(new_value);
+            }
+            let diff = make_diff(&orig_lines.join(""), &new.join(""));
+            println!("{diff}");
+            let patch = Patch::from_single(&diff).unwrap();
+            let affected = affected_lines(&patch);
+            // When we insert a line then only that one line is affected.
+            assert_eq!(affected, &[i]);
+        }
+    }
+
+    #[test]
+    fn affected_lines_from_single_deletion() {
+        let orig_lines = (1..=5).map(|i| format!("line {}\n", i)).collect_vec();
+        for i in 1..=5 {
+            let mut new = orig_lines.clone();
+            new.remove(i - 1);
+            let diff = make_diff(&orig_lines.join(""), &new.join(""));
+            println!("{diff}");
+            let patch = Patch::from_single(&diff).unwrap();
+            let affected = affected_lines(&patch);
+            // If line 1 is removed we should see line 1 as affected. If line 2 is removed
+            // then 1 and 2 are affected, etc. If line 5 is removed, then only 4, the last
+            // remaining line is affected.
+            match i {
+                1 => assert_eq!(affected, &[1]),
+                5 => assert_eq!(affected, &[4]),
+                i => assert_eq!(affected, &[i - 1, i]),
+            }
+        }
+    }
+
+    #[test]
+    fn affected_lines_from_double_deletion() {
+        let orig_lines = (1..=5).map(|i| format!("line {}\n", i)).collect_vec();
+        for i in 1..=4 {
+            let mut new = orig_lines.clone();
+            new.remove(i - 1);
+            new.remove(i - 1);
+            let diff = make_diff(&orig_lines.join(""), &new.join(""));
+            println!("{diff}");
+            let patch = Patch::from_single(&diff).unwrap();
+            let affected = affected_lines(&patch);
+            match i {
+                1 => assert_eq!(affected, &[1]),
+                4 => assert_eq!(affected, &[3]),
+                2 | 3 => assert_eq!(affected, &[i - 1, i]),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[test]
+    fn affected_lines_from_replacement() {
+        let orig_lines = (1..=5).map(|i| format!("line {}\n", i)).collect_vec();
+        for i in 1..=5 {
+            let insertion = ["new 1\n".to_owned(), "new 2\n".to_owned()];
+            let new = orig_lines[..(i - 1)]
+                .iter()
+                .cloned()
+                .chain(insertion)
+                .chain(orig_lines[i..].iter().cloned())
+                .collect_vec();
+            let diff = make_diff(&orig_lines.join(""), &new.join(""));
+            println!("{diff}");
+            let patch = Patch::from_single(&diff).unwrap();
+            let affected = affected_lines(&patch);
+            if i > 1 {
+                // The line before the deletion also counts as affected.
+                assert_eq!(affected, &[i - 1, i, i + 1]);
+            } else {
+                assert_eq!(affected, &[i, i + 1]);
+            }
+        }
     }
 }
