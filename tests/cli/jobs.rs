f829110ab539ed878f2960097515e2f5b12a43ee
Martin Pool <mbp@sourcefrog.net>
2023-12-10 19:10:18 -0800

    Run the jobs test on a smaller tree for speed

--- tests/cli/jobs.rs
+++ tests/cli/jobs.rs
@@ -5,15 +5,16 @@
 use super::{copy_of_testdata, run};
 
 /// It's a bit hard to assess that multiple jobs really ran in parallel,
-/// but we can at least check that the option is accepted.
+/// but we can at least check that the option is accepted, and see that the
+/// debug log looks like it's using multiple threads.
 #[test]
 fn jobs_option_accepted() {
-    let testdata = copy_of_testdata("well_tested");
+    let testdata = copy_of_testdata("small_well_tested");
     run()
         .arg("mutants")
         .arg("-d")
         .arg(testdata.path())
-        .arg("-j3")
+        .arg("-j2")
         .arg("--minimum-test-timeout=120") // to avoid flakes on slow CI
         .assert()
         .success();
