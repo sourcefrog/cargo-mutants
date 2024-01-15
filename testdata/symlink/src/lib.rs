ac3879525a80f2ec844794d857224985ba1e5842
Martin Pool <mbp@sourcefrog.net>
2024-01-15 14:48:52 -0800

    Try to make symlink testdata more robust

--- testdata/symlink/src/lib.rs
+++ testdata/symlink/src/lib.rs
@@ -1,15 +1,7 @@
 use std::path::Path;
 
-fn read_through_symlink() -> String {
+pub fn read_through_symlink() -> String {
     let path = Path::new("testdata/symlink");
     assert!(path.is_symlink());
     std::fs::read_to_string(path).unwrap()
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn read_through_symlink_test() {
-        assert_eq!(super::read_through_symlink().trim(), "Hello, world!");
-    }
 }
