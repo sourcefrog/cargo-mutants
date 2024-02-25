01ce63842141bb2f9a864efc78574295e41a685a
Martin Pool <mbp@sourcefrog.net>
2024-02-25 08:09:09 -0800

    Fix Windows filter test
    Only run Windows tests on Windows

--- tests/windows.rs
+++ tests/windows.rs
@@ -1,15 +1,25 @@
 // Copyright 2021-2023 Martin Pool
 
+#![cfg(windows)]
+
 //! Windows-only CLI tests.
 
-mod util;
-use util::{run, CommandInstaExt};
+use predicates::prelude::*;
 
+mod util;
+use util::run;
+
+/// Only on Windows, backslash can be used as a path separator in filters.
 #[test]
 fn list_mutants_well_tested_exclude_folder_containing_backslash_on_windows() {
     run()
         .arg("mutants")
         .args(["--list", "--exclude", "*\\module\\*"])
         .current_dir("testdata/with_child_directories")
-        .assert_insta("list_mutants_well_tested_exclude_folder_filter");
+        .assert()
+        .stdout(
+            predicates::str::contains(r"src/module")
+                .not()
+                .and(predicates::str::contains(r"src/methods.rs")),
+        );
 }
