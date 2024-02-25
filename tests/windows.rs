47b97549e60ade11df9791a268a624aae771cdff
Martin Pool <mbp@sourcefrog.net>
2024-02-25 08:01:18 -0800

    Move all tests directly inside tests/
    Fixes #278

---
+++ tests/windows.rs
@@ -0,0 +1,15 @@
+// Copyright 2021-2023 Martin Pool
+
+//! Windows-only CLI tests.
+
+mod util;
+use util::{run, CommandInstaExt};
+
+#[test]
+fn list_mutants_well_tested_exclude_folder_containing_backslash_on_windows() {
+    run()
+        .arg("mutants")
+        .args(["--list", "--exclude", "*\\module\\*"])
+        .current_dir("testdata/with_child_directories")
+        .assert_insta("list_mutants_well_tested_exclude_folder_filter");
+}
